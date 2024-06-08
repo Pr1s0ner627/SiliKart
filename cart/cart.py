c0149f9e443746bf115aa94f1927eb7121c019ef
Pr1s0ner627 <g1mw9827@gmail.com>
2024-06-08 19:32:55 +0530

    Cart is coming along

--- cart/cart.py
+++ cart/cart.py
@@ -1,3 +1,5 @@
+from store import models    
+
 class Cart():
     def __init__(self, request):
         self.session = request.session
@@ -6,14 +8,24 @@ class Cart():
             cart = self.session['session_key'] ={}
         self.cart = cart
     
-    def add(self, product):
+    def add(self, product, quantity):
         product_id = str(product.id)
+        product_qty = str(quantity)
         if product_id in self.cart:
             pass
         else:
-            self.cart[product_id] = {'price':str(product.ProdPrice)}
-        
+            # self.cart[product_id] = {'price':str(product.ProdPrice)}
+            self.cart[product_id] = int(product_qty)
         self.session.modified = True
     
     def __len__(self):
         return len(self.cart)
+    
+    def getProd(self):
+        product_id = self.cart.keys()
+        products = models.Product.objects.filter(id__in=product_id)
+        return products
+    
+    def getQuants(self):
+        quantities = self.cart
+        return quantities
--- cart/views.py
+++ cart/views.py
@@ -4,14 +4,18 @@ from store import models
 from django.http import JsonResponse
 # Create your views here.
 def cartSummary(request):
-    return render(request, 'cartSummary.html', {})
+    cart = Cart(request)
+    cart_prods = cart.getProd
+    quantities = cart.getQuants
+    return render(request, 'cartSummary.html', {'cart_products':cart_prods, 'quantities':quantities})
 
 def cartAdd(request):
     cart = Cart(request)
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
         product = get_object_or_404(models.Product, id=product_id)
-    cart.add(product=product)
+    cart.add(product=product, quantity=product_qty)
     cart_quantity = cart.__len__()
     # response = JsonResponse({'Name':product.ProdName})
     # return response
